e58311966d1b4b3451c85a26fdce993db6d65fb2
moatp1 <93752235+moatp1@users.noreply.github.com>
2022-05-08 16:26:11 +0700

    Add files via upload

---
+++ Web_TH/src/main/java/com/example/demo/web/EditController.java
@@ -0,0 +1,23 @@
+package com.example.demo.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.demo.Product;
+import com.example.demo.data.ProductRepository;
+import java.util.*;
+import antlr.collections.List;
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+@Controller
+@RequestMapping("/edit")
+public ModelAndView showEditProductPage(@PathVariable(name = "id") String id) {
+    ModelAndView mav = new ModelAndView("edit_product");
+    Product product = service.get(id);
+    mav.addObject("product", product);
+     
+    return mav;
+}
