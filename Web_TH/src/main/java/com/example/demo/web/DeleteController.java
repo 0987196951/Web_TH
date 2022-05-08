a258bdc9c5001d6602c15fbc7dcf34089b4e3705
moatp1 <93752235+moatp1@users.noreply.github.com>
2022-05-08 23:04:22 +0700

    Add files via upload

---
+++ Web_TH/src/main/java/com/example/demo/web/DeleteController.java
@@ -0,0 +1,19 @@
+package com.example.demo.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/delete")
+public class DeleteController {
+	
+	@Autowired
+	private ProductService service;
+	@RequestMapping("/delete/{id}")
+	public String deleteProduct(@PathVariable(name = "id") int id) {
+	    service.delete(id);
+	    return "redirect:/";       
+	}
+}
